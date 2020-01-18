799a634c4065a16465cb47358729a223b38fc3a5
Scott Rogers <scottrogers@newrelic.com>
2020-01-18 05:53:29 -0500

    renamed ceviewer to index.js

--- nerdlets/ce-v2-nerdlet/index.js
+++ nerdlets/ce-v2-nerdlet/index.js
@@ -1,43 +1,70 @@
-import { AccountsQuery, PlatformStateContext, NerdletStateContext, AutoSizer } from 'nr1';
-import CeViewer from './ceviewer';
-import { getCustomEventList } from './utils';
+import React from 'react';
+import { Spinner, TableChart, Grid, GridItem, PlatformStateContext } from 'nr1';
+import AccountPicker from './account-picker'
+import { genTableDataV2 } from './utils';
 
-export default class Wrapper extends React.PureComponent {
+export default class CeViewer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAccountId: null,
+      tableData: [],
+      isLoading: true
+    };
+    this.onAccountSelected = this.onAccountSelected.bind(this);
+  }
+
+  async componentDidUpdate() {
+    // account-picker callback seems a better choice
+  }
+
+  async onAccountSelected(accountId) {
+    // await console.debug(">>>>>>>>>>>> onAccountSelected fired <<<<<<<<<<<<<<<");
+    await console.debug("accountId: " + accountId);
+    this.setState({ selectedAccountId: accountId });
+    this.setState({ isLoading: true });
+    // const { timeRange }  = this.props.launcherUrlState;
+    const tableData = await genTableDataV2(this.state.selectedAccountId);
+    this.setState({ tableData, isLoading: false });
+  }
 
   render() {
-    return (
-              <AutoSizer>
-                {({ width, height }) => (
-                  <CeViewer
-                    // launcherUrlState={launcherUrlState}
-                    // nerdletUrlState={nerdletUrlState}
-                    // width={width}
-                    // height={height}
-                  />
-                )}
-              </AutoSizer>
-    );
+    console.debug("++++++++++ render fired ++++++++++++ ");
+    // console.debug(JSON.stringify(this.state.tableData));
+    if (!this.state.isLoading) {
+      // console.debug(this.state.tableData[0].data.length);
+      let chartData = this.state.tableData;
+      return (
+        <>
+          <Grid spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]} >
+            <GridItem>
+              <AccountPicker accountChangedCallback={this.onAccountSelected} />
+            </GridItem>
+          </Grid>
+          <PlatformStateContext.Consumer>
+            {(platformState) => {
+              return(
+                console.debug("hello"),
+                <TableChart data={chartData} fullWidth fullHeight className="" /> 
+              );
+            }}
+            
+          </PlatformStateContext.Consumer>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Grid spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]} >
+            <GridItem>
+              <AccountPicker accountChangedCallback={this.onAccountSelected} />
+            </GridItem>
+          </Grid>
+          <Spinner />
+        </>
+      );
+    }
   }
-  // render() {
-  //   return (
-  //     <PlatformStateContext.Consumer>
-  //       {launcherUrlState => (
-  //         <NerdletStateContext.Consumer>
-  //           {nerdletUrlState => (
-  //             <AutoSizer>
-  //               {({ width, height }) => (
-  //                 <CeViewer
-  //                   launcherUrlState={launcherUrlState}
-  //                   nerdletUrlState={nerdletUrlState}
-  //                   width={width}
-  //                   height={height}
-  //                 />
-  //               )}
-  //             </AutoSizer>
-  //           )}
-  //         </NerdletStateContext.Consumer>
-  //       )}
-  //     </PlatformStateContext.Consumer>
-  //   );
-  // }
 }
+
